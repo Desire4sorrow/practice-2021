9eeeba8b789a03a9de43f80130fe6dc8ae95ca6d
Desire4sorrow <56004655+Desire4sorrow@users.noreply.github.com>
2021-07-15 22:49:40 +0300

    finished Briefs page, Added 1st stage of projectinfo

--- src/briefs.js
+++ src/briefs.js
@@ -4,12 +4,12 @@ import {
     ReferenceInput,
     SelectInput,
     TextInput,
-        RadioButtonGroupInput,
+    CheckboxGroupInput,
+    RadioButtonGroupInput,
 } from 'react-admin';
 
 export const BriefList = props => (
     <SimpleForm>
-
         <label>Электронная почта</label>
         <TextInput source="Адрес электронной почты" resettable />
         <label>Как называется Ваша компания?</label>
@@ -39,15 +39,30 @@ export const BriefList = props => (
         <label>Кто его целевая аудитория?</label>
         <TextInput source="Развернутый ответ" resettable/>
         <label>Есть ли какая-либо документация (ТЗ, спецификации, макеты экранов, интерактивный прототип)?</label>
-            <RadioButtonGroupInput source="Выберите один вариант ответа" choices={[
+            <RadioButtonGroupInput source="Documentation" choices={[
                     { id: 'yesDoc', name: 'Да' },
                     { id: 'noDoc', name: 'Нет' },
                     { id: 'otherDoc', name: 'Другое' }
-            ]} />
+            ]} row={false} />
         <label>Укажите платформы, которые должны поддерживаться</label>
+        <CheckboxGroupInput source="Platforms" choices={[
+            { id: 'IOS', name: 'IOS(Iphone или Ipad)' },
+            { id: 'Iphone', name: 'Iphone only' },
+            { id: 'Ipad', name: 'Ipad only' },
+            { id: 'AppleWatch', name: 'Apple Watch'},
+            { id: 'AppleTV', name: 'AppleTV'},
+            { id: 'AndroidOnly', name: 'Android smartphones only'},
+            { id: 'AndroidTablets', name: 'Android tablets only'},
+            { id: 'SmartphonesTablets', name: 'Android smartphones and tablets'},
+            { id: 'Web', name: 'Web'},
+        ]} row={false}/>
             <label> Если есть, то могли бы Вы предоставить файлы для ознакомления? (пожалуйста, укажите ссылку на файлы)</label>
                     <TextInput source="Развернутый ответ" resettable/>
         <label>Укажите версии OS</label>
+        <CheckboxGroupInput source="OsVersion" choices={[
+            { id: 'IOS8+', name: 'IOS 8+' },
+            { id: 'Android4+', name: 'Android 4+' },
+        ]} row={false}/>
             <label>Перечислите ссылки на подобные проекты, существующие на рынке. </label>
             <TextInput source="Развернутый ответ" resettable/>
         <label>Укажите, какие, на Ваш взгляд, у них преимущества и недостатки.</label>
@@ -55,40 +70,62 @@ export const BriefList = props => (
             <label>Какие конкурентные примущества будут у вашего проекта? </label>
             <TextInput source="Развернутый ответ" resettable/>
             <label>Будет ли предоставляться дизайн, или его необходимо создать с нуля? </label>
+        <CheckboxGroupInput source="Design" choices={[
+            { id: 'NoDesign', name: 'Нет, нам нужно создать дизайн с нуля' },
+            { id: 'IconsDesign', name: 'У нас есть иконки приложения' },
+            { id: 'FinDesign', name: 'У нас есть окончательный дизайн, и наш дизайнер уже подготовил все ресурсы для разработки в соответствии с вашими стандартами'}
+        ]} row={false}/>
             <label>Будет ли приложение включать фоновую музыку и звуки?</label>
+        <CheckboxGroupInput source="Music" choices={[
+            { id: 'MusicYes', name: 'Да' },
+            { id: 'MusicNo', name: 'Нет' },
+            { id: 'MusicYesExist', name: 'Да, и Клиент предоставит аудио файлы' },
+            { id: 'MusicYesNotExist', name: 'Да, Клиенту необходимо, чтобы они были созданы как часть этого заказа'},
+            { id: 'MusicNotStated', name: 'Пока не уверен'},
+        ]} row={false}/>
             <label>Будет ли приложение многоязычным? Какие языки оно должно поддерживать? </label>
             <TextInput source="Развернутый ответ" resettable/>
             <label>Какую модель монетизации будет использовать приложение?</label>
+        <CheckboxGroupInput source="Monetization" choices={[
+            { id: 'MonetizationPaid', name: 'Платное' },
+            { id: 'MonetizationFree', name: 'Бесплатное' },
+            { id: 'MonetizationFreePaid', name: 'Будет включать в себя покупки в приложении' },
+            { id: 'MonetizationNotStated', name: 'Еще не знаю'},
+        ]} row={false}/>
             <label>Будет ли серверная часть, панель администратора? </label>
+                <RadioButtonGroupInput source="Backend part" choices={[
+            { id: 'BackendPartYes', name: 'Да' },
+            { id: 'BackendPartNo', name: 'Нет' },
+                ]} row={false}/>
             <label>Если да, то будет ли предоставляться бэкэнд, API?</label>
-            <RadioButtonGroupInput source="Выберите один вариант ответа" choices={[
-                    { id: 'yesApi', name: 'Да' },
-                    { id: 'noApi', name: 'Нет' },
-            ]} />
+            <RadioButtonGroupInput source="Backend" choices={[
+                { id: 'yesApi', name: 'Да' },
+                { id: 'noApi', name: 'Нет' },
+            ]} row={false}/>
             <label>Пожалуйста, укажите режим экрана, который будет поддерживаться в приложении.</label>
-            <RadioButtonGroupInput source="Выберите один вариант ответа" choices={[
+            <RadioButtonGroupInput source="Screen" choices={[
                     { id: 'port', name: 'Портретный' },
                     { id: 'land', name: 'Пейзажный' },
                     { id: 'portland', name: 'Портретный и Пейзажный'}
-            ]} />
+            ]} row={false}/>
             <label>Нужна ли вам помощь при публикации в AppStore / Google Play?</label>
-            <RadioButtonGroupInput source="Выберите один вариант ответа" choices={[
+            <RadioButtonGroupInput source="Publication" choices={[
                     { id: 'yesPub', name: 'Да' },
                     { id: 'noPub', name: 'Нет' },
-            ]} />
+            ]} row={false}/>
             <label>Предполагаемая дата старта разработки </label>
             <TextInput source="Краткий ответ" resettable/>
             <label>Предполагаемая дата релиза </label>
             <TextInput source="Краткий ответ" resettable/>
             <label>Выделенный бюджет </label>
-            <RadioButtonGroupInput source="Выберите один вариант ответа" choices={[
+            <RadioButtonGroupInput source="Budjet" choices={[
                     { id: 'before1', name: 'До 1 млн рублей' },
                     { id: '1-3', name: '1 млн рублей - 3 млн рублей' },
                     { id: '3-6', name: '3 млн рублей - 6 млн рублей'},
                     { id: '6-10', name: '6 млн рублей - 10 млн рублей'},
                     { id: '10-15', name: '10 млн рублей - 15 млн рублей'},
                     { id: '15+', name: '15 млн рублей +'},
-            ]} />
+            ]} row={false}/>
             <label>Прочие комментарии/ нюансы, которые должна учитывать команда  </label>
             <TextInput source="Развернутый ответ" resettable/>
     </SimpleForm>
