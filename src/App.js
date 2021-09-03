856edb3a62575461e637b0ef8d0c63a8b46a1280
Ruslan <56004655+Desire4sorrow@users.noreply.github.com>
2021-09-03 19:58:09 +0300

    corrected port and type of logination

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { fetchUtils, Admin, Resource } from 'react-admin';
 import { ProjectList, ProjectEdit, ProjectCreate, ProjectShow } from './projects';
 import {BriefList} from "./briefs";
 import Dashboard from './Dashboard';
-import authProvider from './embeddedModules/authProvider';
+import {authProvider, httpClient} from './embeddedModules/authProvider';
 import NotFound from './embeddedModules/NotFound';
 import {NewsList, NewsCreate} from "./telegram";
 
@@ -14,22 +14,12 @@ import PostIcon from '@material-ui/icons/Book';
 import BriefIcon from '@material-ui/icons/ChromeReaderMode';
 import NewsIcon from '@material-ui/icons/Announcement';
 
-const httpClient = (url, options = {}) => {
-    if (!options.headers) {
-        options.headers = new Headers({ Accept: 'application/json' });
-    }
-    const { token } = JSON.parse(localStorage.getItem('auth'));
-    options.headers.set('Authorization', `Bearer ${token}`);
-    return fetchUtils.fetchJson(url, options);
-};
-
-const dataProvider = jsonServerProvider('http://127.0.0.1:3001', httpClient);
 
 const App = () => (
     <Admin catchAll={NotFound}
            dashboard={Dashboard}
            authProvider={authProvider}
-           dataProvider={dataProvider}>
+           dataProvider={jsonServerProvider('http://127.0.0.1:3001', httpClient)}>
 
         <Resource name="projects" list={ProjectList} edit={ProjectEdit} create={ProjectCreate} show={ProjectShow} icon={PostIcon} />
         <Resource name="briefs"  list={BriefList} icon={BriefIcon} />
--- src/embeddedModules/authProvider.js
+++ src/embeddedModules/authProvider.js
@@ -1,31 +1,36 @@
-export default {
-    // called when the user attempts to log in
-    login: ({ username, password }) =>  {
-        const request = new Request('https://127.0.0.1:3001', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: new Headers({ 'Content-Type': 'application/json' }),
-        });
+export const httpClient = () => {
+    const { token } = JSON.parse(localStorage.getItem('auth')) || {};
+    return { Authorization: `Bearer ${token}` };
+};
+
+export const authProvider = {
+    // authentication
+    login: ({ username, password }) => {
+        const request = new Request(
+            process.env.REACT_APP_API_URL + '/users/login',
+            {
+                method: 'POST',
+                body: JSON.stringify({ email: username, password }),
+                headers: new Headers({ 'Content-Type': 'application/json' }),
+            }
+        );
         return fetch(request)
-            .then(response => {
+            .then((response) => {
                 if (response.status < 200 || response.status >= 300) {
                     throw new Error(response.statusText);
                 }
                 return response.json();
             })
-            .then(auth => {
-                localStorage.setItem('auth', JSON.stringify(auth));
+            .then((auth) => {
+                localStorage.setItem(
+                    'auth',
+                    JSON.stringify({ ...auth, fullName: username })
+                );
             })
             .catch(() => {
-                throw new Error('Network error')
+                throw new Error('Network error');
             });
     },
-    // called when the user clicks on the logout button
-    logout: () => {
-        localStorage.removeItem('username');
-        return Promise.resolve();
-    },
-    // called when the API returns an error
     checkError: (error) => {
         const status = error.status;
         if (status === 401 || status === 403) {
@@ -35,12 +40,21 @@ export default {
         // other error code (404, 500, etc): no need to log out
         return Promise.resolve();
     },
-    // called when the user navigates to a new location, to check for authentication
-    checkAuth: () => {
-        return localStorage.getItem('username')
+    checkAuth: () =>
+        localStorage.getItem('auth')
             ? Promise.resolve()
-            : Promise.reject();
+            : Promise.reject({ message: 'login required' }),
+    logout: () => {
+        localStorage.removeItem('auth');
+        return Promise.resolve();
     },
-    // called when the user navigates to a new location, to check for permissions / roles
-    getPermissions: () => Promise.resolve(),
+    getIdentity: () => {
+        try {
+            const { id, fullName, avatar } = JSON.parse(localStorage.getItem('auth'));
+            return Promise.resolve({ id, fullName, avatar });
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    },
+    getPermissions: (params) => Promise.resolve(),
 };
